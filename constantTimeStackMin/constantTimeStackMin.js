72f42b00b45f797ee1b974ac1899b13cacb87a2c
Gregory <gregoryinouye@gmail.com>
2019-04-19 09:50:04 -0700

    working constant time stack but min does not update if min value is removed

--- constantTimeStackMin/constantTimeStackMin.js
+++ constantTimeStackMin/constantTimeStackMin.js
@@ -18,23 +18,41 @@
   * Stack Class
   */
   var Stack = function() {
+    this.numItems = 0;
+    this.minimum = undefined;
+    // this.minKeys = [];
+    this.list = {};
 
   // add an item to the top of the stack
     this.push = function(value) {
+      this.minimum = this.minimum ? Math.min(this.minimum, value) : value;
+      this.numItems++;
+      this.list[this.numItems] = value;
     };
 
   // remove an item from the top of the stack
     this.pop = function() {
+      if (this.numItems === 0) {
+        return 'no items in stack';
+      }
+      let popped = this.list[this.numItems];
+      delete this.list[this.numItems];
+      this.numItems--;
+      // update min
+      if (this.minimum === popped) {
+        this.minimum = undefined;
+      }
+      return popped;
     };
 
   // return the number of items in the stack
     this.size = function() {
+      return this.numItems;
     };
   
   // return the minimum value in the stack
     this.min = function() {
-
+      return this.minimum;
     };
-
   };
 
