cdad2aa54b4a3313f0de3978499549e123be2097
Hack Reactor Student <student@hackreactor.com>
2019-05-01 09:45:37 -0700

    completed allAnagrams and extra credit deduplication

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,6 +12,40 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
+// var allAnagrams = function(string) {
+//   let resultArray = [];
+//   let getAnagrams = function(current, remaining) {
+//     if (remaining.length === 1) {
+//       resultArray.push(current.push(remaining[0]).join(''));
+//     } else {
+//       remaining.forEach((letter, index) => getAnagrams(current.push(letter), remaining.splice(index, 1)));
+//     }
+//   };
+//   getAnagrams([], [...string]);
+//   return resultArray;
+// };
+
+
 var allAnagrams = function(string) {
-  // Your code here.
+  let resultArray = [];
+  let getAnagrams = function(current, remaining) {
+    if (remaining.length === 1) {
+      resultArray.push(current + remaining);
+    } else {
+      for (let i = 0; i < remaining.length; i++) {
+        getAnagrams(current + remaining.slice(i, i + 1), remaining.slice(0, i) + remaining.slice(i + 1, remaining.length));
+      }
+    }
+  };
+
+  getAnagrams('', string);
+
+  let resultSet = new Set();
+  resultArray.forEach(item => resultSet.add(item));
+  let finalResult = [];
+  resultSet.forEach(item => finalResult.push(item));
+
+  return finalResult;
 };
+
+// time complexity: quadratic
