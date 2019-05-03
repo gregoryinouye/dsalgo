69c6919ee90c758d7595ff48621f9bbfd2437ee9
Hack Reactor Student <student@hackreactor.com>
2019-05-03 10:33:56 -0700

    refactored

--- coinSums/coinSums.js
+++ coinSums/coinSums.js
@@ -30,29 +30,31 @@ makeChange(2) === 2
 
 var makeChange = function(total) {
 
-  var calcValue = function(qtyArr) {
+  let calcValue = function(qtyArr) {
     let valArr = [200, 100, 50, 20, 10, 5, 2, 1];
-    return qtyArr.reduce((accumulator, currVal, currIndex) => {
-      return currVal * valArr[currIndex] + accumulator, 0;
-    });
+    return qtyArr.reduce((accumulator, currVal, currIndex) => currVal * valArr[currIndex] + accumulator, 0);
   }
 
   let solutions = 0;
 
-  var makeChangeRecurser = function(currArr, index) {
+  let makeChangeRecurser = function(currArr, index) {
     currArr[index] += 1;
     currentTotal = calcValue(currArr);
+
     if (currentTotal < total) {
       makeChangeRecurser(currArr, index);
     }
+
     if (currentTotal === total) {
       solutions += 1;
     }
+
     currArr[index] -= 1;
+
     if (index < 7) {
     makeChangeRecurser(currArr, index + 1);
     }
-  };
+  }
 
   makeChangeRecurser([0, 0, 0, 0, 0, 0, 0, 0], 0);
   return solutions;
