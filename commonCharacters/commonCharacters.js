4030e11d52c7d40ff9d63a1bf9a3934cf9b891f9
Hack Reactor Student <student@hackreactor.com>
2019-05-17 09:18:00 -0700

    add commonCharacters

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -10,8 +10,24 @@
  * Extra credit: Extend your function to handle more than two input strings.
  */
 
+// var commonCharacters = function(string1, string2) {
+//   let result = '';
+//   for (let letter of string1) {
+//     if (string2.includes(letter)) {
+//       result = result.concat(letter);
+//     }
+//   }
+//   return result;
+// };
 
+var commonCharacters = function(string1, ...strings) {
+  let result = '';
 
-var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+  for (let letter of string1) {
+    if (strings.every(element => element.includes(letter))) {
+      result = result.concat(letter);
+    }
+  }
+
+  return result;
 };
