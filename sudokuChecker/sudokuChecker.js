9c6c4e0493c1890f3126ed37e6a9923a3d29f5c9
Gregory <gregoryinouye@gmail.com>
2019-06-05 09:47:31 -0700

    sudokuChecker

--- sudokuChecker/sudokuChecker.js
+++ sudokuChecker/sudokuChecker.js
@@ -18,9 +18,27 @@ Example input:
 157638429"
 */
 
-
+// const board = "735814296\n896275314\n214963857\n589427163\n362189745\n471356982\n923541678\n648792531\n157638429";
 
 function sudokuChecker(board) {
-  // Your code here.
-  
+  return [0, 1, 2, 3, 4, 5, 6, 7, 8].every(element => checkAllNums(getColumn(board, element)) && checkAllNums(getRow(board, element)) && checkAllNums(getGrid(board, element)));
 }
+
+const getColumn = (board, col) => {
+  let column = '';
+  for (let i = 0; i < 9; i++) {
+    column += board[10 * i + col];
+  }
+  return column;
+}
+
+const getRow = (board, row) => {
+  return board.slice(row * 10, row * 10 + 9);
+}
+
+const getGrid = (board, grid) => {
+  const firstPosition = [0, 3, 6, 30, 33, 36, 60, 63, 66][grid];
+  return board.slice(firstPosition, firstPosition + 3) + board.slice(firstPosition + 10, firstPosition + 13) + board.slice(firstPosition + 20, firstPosition + 23);
+}
+
+const checkAllNums = ([...set]) => ['1', '2', '3', '4', '5', '6', '7', '8', '9'].every(element => set.includes(element));
