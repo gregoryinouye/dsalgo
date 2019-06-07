4191960634e43da9a1094698a7d7dbc28a3d0818
Gregory <gregoryinouye@gmail.com>
2019-06-07 09:47:08 -0700

    add non-recursive solution

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -9,23 +9,36 @@
 *
 */
 
-function reverseInteger(number, powerOfTen){
+// NON-RECURSIVE SOLUTION
 
-  if (number < 10) {
-    return number;
+const reverseInteger = (number) => {
+  let output = 0;
+  while (number > 0) {
+    output = output * 10 + number % 10;
+    number = Math.floor(number / 10);
   }
+  return output;
+}
 
-  if (powerOfTen === undefined) {
-    powerOfTen = 0;
-    while (number / Math.pow(10, powerOfTen + 1) > 1) {
-      powerOfTen++;
-    }
-  }
+// RECURSIVE SOLUTION
 
-  for (let i = 0; i < 10; i++) {
-    if ((number - i) % 10 === 0) {
-      // determine length of number to figure out how many 10 ^ n
-      return i * Math.pow(10, powerOfTen) + reverseInteger((number - i) / 10, powerOfTen - 1);
-    }
-  }
-};
+// function reverseInteger(number, powerOfTen){
+
+//   if (number < 10) {
+//     return number;
+//   }
+
+//   if (powerOfTen === undefined) {
+//     powerOfTen = 0;
+//     while (number / Math.pow(10, powerOfTen + 1) > 1) {
+//       powerOfTen++;
+//     }
+//   }
+
+//   for (let i = 0; i < 10; i++) {
+//     if ((number - i) % 10 === 0) {
+//       // determine length of number to figure out how many 10 ^ n
+//       return i * Math.pow(10, powerOfTen) + reverseInteger((number - i) / 10, powerOfTen - 1);
+//     }
+//   }
+// };
