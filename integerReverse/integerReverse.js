4f617be7f43d35ac37bea89d234b9284a3b15a44
Gregory <gregoryinouye@gmail.com>
2019-06-07 09:52:38 -0700

    removed comments

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -37,7 +37,6 @@ const reverseInteger = (number) => {
 
 //   for (let i = 0; i < 10; i++) {
 //     if ((number - i) % 10 === 0) {
-//       // determine length of number to figure out how many 10 ^ n
 //       return i * Math.pow(10, powerOfTen) + reverseInteger((number - i) / 10, powerOfTen - 1);
 //     }
 //   }
