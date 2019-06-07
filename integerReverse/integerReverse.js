bb86e2fc92b39d49af876b7cb069fde6947e36e8
Gregory <gregoryinouye@gmail.com>
2019-06-07 09:33:05 -0700

    add reverseInteger

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -9,6 +9,23 @@
 *
 */
 
-function reverseInteger(number){
-  // TODO: Implement this function!
-}
+function reverseInteger(number, powerOfTen){
+
+  if (number < 10) {
+    return number;
+  }
+
+  if (powerOfTen === undefined) {
+    powerOfTen = 0;
+    while (number / Math.pow(10, powerOfTen + 1) > 1) {
+      powerOfTen++;
+    }
+  }
+
+  for (let i = 0; i < 10; i++) {
+    if ((number - i) % 10 === 0) {
+      // determine length of number to figure out how many 10 ^ n
+      return i * Math.pow(10, powerOfTen) + reverseInteger((number - i) / 10, powerOfTen - 1);
+    }
+  }
+};
