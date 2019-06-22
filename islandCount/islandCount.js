b4459f206c6805032b788c9054694726d56ab925
Gregory <gregoryinouye@gmail.com>
2019-06-22 09:35:23 -0700

    islandCount

--- islandCount/islandCount.js
+++ islandCount/islandCount.js
@@ -6,5 +6,43 @@
  */
 
 function countIslands(mapStr) {
-  // Your code here.
-}
+  const map = mapStr.split('\n').map(element => [...element]);
+  
+  const isUpLand = (i, j) => map[i - 1] ? map[i - 1][j] === '.' : false;
+  const isDownLand = (i, j) => map[i + 1] ? map[i + 1][j] === '.' : false;
+  const isRightLand = (i, j) => map[i][j + 1] === '.';
+  const isLeftLand = (i, j) => map[i][j - 1] === '.';
+
+  const findConnectedLand = (i, j) => {
+    if (map[i][j] !== '.') {
+      return;
+    } else {
+      map[i][j] = 1;
+      if (isUpLand(i, j)) {
+        findConnectedLand(i - 1, j);
+      }
+      if (isDownLand(i, j)) {
+        findConnectedLand(i + 1, j);
+      }
+      if (isRightLand(i, j)) {
+        findConnectedLand(i, j + 1);
+      }
+      if (isLeftLand(i, j)) {
+        findConnectedLand(i, j - 1);
+      }
+    }
+  }
+
+  let islandCount = 0;
+
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[0].length; j ++) {
+      if (map[i][j] === '.') {
+        islandCount++;
+        findConnectedLand(i, j);
+      }
+    }
+  }
+  
+  return islandCount;
+};
