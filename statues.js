0d6cdfc5c0f3a4c0eb9e2a345cbb83992f915766
Gregory Inouye <aznrunner@gmail.com>
2019-10-11 13:35:02 -0700

    add statues problem desc

---
+++ statues.js
@@ -0,0 +1,19 @@
+/*
+
+Given array of statue heights, and an array of cave heights, find max number of statues that can fit in the cave.
+
+You can only insert into the cave from the opening (which is at index 0), and slide as necessary.
+
+For example,
+statues = [1,2,3]
+cave = [2,1,1]
+
+Answer: 2 because statue of height 1 can slide all the way to the back of the cave, and statue of height 2 can slide into the opening of the cave.
+
+Another example,
+statues = [5]
+cave = [1,20,20]
+
+Answer: 0 because the statue of height 5 cannot get past the opening of the cave (which is height 1).
+
+*/
