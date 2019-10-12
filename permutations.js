100ba090c3be306a9357b3b47a5fd78292fa3cfe
Gregory Inouye <aznrunner@gmail.com>
2019-10-12 16:22:34 -0700

    revise permutations description

--- permutations.js
+++ permutations.js
@@ -2,14 +2,10 @@
 
 const dict = { '1' : [a, b], '2': [c, d] };
 
-create a function that if given an input string
-spits out the permutations
-so if you're given '12'
-the output is
-[ac, ad, bc, bd]
-that's the first part
-the next part is
-not assume you're given an input '1234' (assume all keys exist)
+Given an input string, return the permutations such that if you are given '12'
+the output is [ac, ad, bc, bd].
+
+Assume you're given an input '1234' (and assume all keys exist)
 instead of it just being 1,2,3,4
 it's actually
 1,2,3,4 1,2,34, 1,23,4 1,234 12,3,4 12,34 123,4 1234
