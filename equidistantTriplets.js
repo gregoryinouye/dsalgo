0501417f9c1cc3c59a84da4426ea5fcd84a4ff8f
Gregory <gregoryinouye@gmail.com>
2020-01-04 13:17:12 -0800

    add prompt for equidistantTriplets

---
+++ equidistantTriplets.js
@@ -0,0 +1,14 @@
+/*
+
+Given a list of numbers, find the number of equidistant triples.
+
+An equidistant triple exists if one number is located at the midpoint between two other numbers.
+
+example:
+
+input = [1, 2, 4, 6, 7, 8]
+output = 4
+
+The equidistant triples are (1, 4, 7), (2, 4, 6), (4, 6, 8), (6, 7, 8).
+
+*/
